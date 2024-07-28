63f6ae455a4c2e2b75309654cd5883b6270a17c0
Bao Nhat <nguyennhatnguyen2@gmail.com>
2024-07-28 17:06:24 +0700

    commit all

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./routes/layout/layout";
+import HomePage from "./routes/homePage/homePage";
+import ListPage from "./routes/listPage/listPage";
+import SinglePage from "./routes/singlePage/singlePage";
+import Login from "./routes/login/login";
+import Register from "./routes/register/register";
+
+function App() {
+   const router = createBrowserRouter([
+      {
+         path: "/",
+         element: <Layout />,
+         children: [
+            {
+               path: "/",
+               element: <HomePage />,
+            },
+            {
+               path: "/list",
+               element: <ListPage />,
+               //loader: listPageLoader,
+            },
+            {
+               path: "/:id",
+               element: <SinglePage />,
+               //loader: singlePageLoader,
+            },
+
+            {
+               path: "/login",
+               element: <Login />,
+            },
+            {
+               path: "/register",
+               element: <Register />,
+            },
+         ],
+      },
+   ]);
+
+   return <RouterProvider router={router} />;
+}
+
+export default App;
